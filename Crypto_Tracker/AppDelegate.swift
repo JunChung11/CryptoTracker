7a7e0ed2e388215c7827773acfc72d6fa7771a90
JunChung11 <jun18shadow@gmail.com>
2024-03-20 20:36:25 -0500

    Progress Checkpoint #1

--- Crypto_Tracker/AppDelegate.swift
+++ Crypto_Tracker/AppDelegate.swift
@@ -10,7 +10,45 @@ import SwiftUI
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    var statusItem: NSStatusItem!
+    
+    private lazy var contentView: NSView? = {
+        let view = (statusItem.value(forKey: "window") as? NSWindow)?.contentView
+        return view
+    }()
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
         print("App Did Finish Launch")
     }
+
+}
+
+// MARK: - MENU BAR
+
+extension AppDelegate {
+    
+    func setupMenuBar() {
+        statusItem = NSStatusBar.system.statusItem(withLength: 64)
+        guard let contentView = self.contentView,
+              let menuButton = statusItem.button
+        else { return }
+        
+        let hostingView = NSHostingView(rootView: MenuBarCoinView())
+        hostingView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(hostingView)
+        
+        NSLayoutConstraint.activate([
+            hostingView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            hostingView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            hostingView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            hostingView.leftAnchor.constraint(equalTo: contentView.leftAnchor)
+        ])
+        
+        menuButton.action = #selector(menuButtonClicked)
+    }
+    
+    @objc func menuButtonClicked() {
+        print("Menu bar item clicked.")
+    }
+
 }
